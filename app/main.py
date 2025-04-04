bfdfe301c723f7bfa0a29c99b57d71a481a71e97
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-04-04 15:39:59 +0200

    Use config file for logging
    Co-authored-by: avinashladdha <63897300+avinashladdha@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -4,24 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from models import UserQuery, SearchResponse
 from search_engine import SemanticSearchEngine
 import json
-import logging
+import logging.config
+from logging.handlers import RotatingFileHandler
+import yaml
+
 
 # --- Logging Configuration ---
-log_file_path = "query_and_response.log"  # Define the log file path
-# Create a rotating file handler
-log_handler = logging.handlers.RotatingFileHandler(
-    log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5)  # 10MB max, 5 backups)
-# Create a formatter
-log_formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(message)s")
-# Set the formatter for the handler
-log_handler.setFormatter(log_formatter)
-# Get the root logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # Set the logging level for the logger
-# Add the handler to the logger
-logger.addHandler(log_handler)
+with open('logging_config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
+logging.config.dictConfig(config)
+logger = logging.getLogger(__name__)
 
 
 app = FastAPI(debug=True)
--- app/search_engine.py
+++ app/search_engine.py
@@ -2,33 +2,25 @@ import torch
 from sentence_transformers import SentenceTransformer, CrossEncoder, util
 import pandas as pd
 from utils import get_value
-import logging
-
-
+import logging.config
+from logging.handlers import RotatingFileHandler
+import yaml
 # --- Logging Configuration ---
-log_file_path = "search_engine.log"  # Define the log file path
-# Create a rotating file handler
-log_handler = logging.handlers.RotatingFileHandler(
-    log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5)  # 10MB max, 5 backups)
-# Create a formatter
-log_formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(message)s")
-# Set the formatter for the handler
-log_handler.setFormatter(log_formatter)
-# Get the root logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # Set the logging level for the logger
-# Add the handler to the logger
-logger.addHandler(log_handler)
+with open('logging_config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+logging.config.dictConfig(config)
+logger = logging.getLogger(__name__)
+
 
 
 
 
 class SemanticSearchEngine:
     def __init__(self, corpus_embeddings_path: str, analysis_df_path: str, package_descr_path: str, device: str = "cpu"):
-        
+
         self.corpus_embeddings = torch.load(corpus_embeddings_path, map_location=torch.device(device))
-        logger.info("Loaded embeddings.") 
+        logger.info("Loaded embeddings.")
         self.bi_encoder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
         self.bi_encoder.max_seq_length = 256
         self.cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
