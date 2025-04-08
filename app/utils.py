9dcef059469a67dd425a68fd8bcf9673d222001b
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-04-08 17:17:50 +0200

    Remove unused function and file
    Follow up from 2cdc5bae91ca4f5f956bb494801838c27cb04c41

--- app/utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def get_value(df: pd.DataFrame, column: str, condition: pd.Series):
-    try:
-        return df.loc[condition, column].iloc[0]
-    except IndexError:
-        return None
