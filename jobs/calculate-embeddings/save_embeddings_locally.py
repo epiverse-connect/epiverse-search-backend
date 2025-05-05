214c93aab5414c2b191d0465e73912ca2ca929af
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-05-05 14:43:48 +0200

    Separate local and azure wrappers for calculate-embeddings

---
+++ jobs/calculate-embeddings/save_embeddings_locally.py
@@ -0,0 +1,16 @@
+import torch
+import logging
+from calculate_embeddings import fetch_docs_and_embed
+
+OUTPUT_EMBEDDINGS_PATH = 'corpus_embeddings.pth'
+OUTPUT_ANALYSIS_DF_PATH = 'analysis_df.csv'
+
+if __name__ == "__main__":
+    analysis_df, corpus_embeddings = fetch_docs_and_embed
+
+    torch.save(corpus_embeddings, OUTPUT_EMBEDDINGS_PATH)
+    logging.info(f"Embeddings saved to '{OUTPUT_EMBEDDINGS_PATH}' with shape: {corpus_embeddings.shape}.")
+
+    analysis_df.to_csv(OUTPUT_ANALYSIS_DF_PATH, index=False)
+    logging.info(
+    f"Analysis DataFrame saved to '{OUTPUT_ANALYSIS_DF_PATH}' with {len(analysis_df)} rows and {len(analysis_df.columns)} columns.")
