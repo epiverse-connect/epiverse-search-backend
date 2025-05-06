e3de81868fa8ae91cd77c93dfce23ced73de0610
bafnaprincy <bafnap@who.int>
2025-05-06 16:34:59 +0200

    tensor to buffer

--- jobs/calculate-embeddings/save_embeddings_azure/__init__.py
+++ jobs/calculate-embeddings/save_embeddings_azure/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import io  
+import tensor
 from calculate_embeddings import fetch_docs_and_embed
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
@@ -16,7 +18,7 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info(f"Using AzureWebJobsStorage: {connection_str}")
 
     blob_service_client = BlobServiceClient.from_connection_string(connection_str)
-    container_name = "embeddings"
+    container_name = "data"
     blob_csv = "analysis_df.csv"
     blob_pth = "corpus_embeddings.pth"
 
@@ -34,4 +36,11 @@ def main(mytimer: func.TimerRequest) -> None:
     blob_csv_client.upload_blob(analysis_df.to_csv(index=False).encode('utf-8'), overwrite=True)
 
     blob_pth_client = container_client.get_blob_client(blob_pth)
-    blob_pth_client.upload_blob(corpus_embeddings, overwrite = True)
+
+    # Serialize tensor to a BytesIO object
+    buffer = io.BytesIO()
+    torch.save(corpus_embeddings, buffer)
+    buffer.seek(0)  # Reset the buffer position to the beginning
+
+    # Upload the buffer to Azure Blob Storage
+    blob_pth_client.upload_blob(buffer, overwrite=True)
