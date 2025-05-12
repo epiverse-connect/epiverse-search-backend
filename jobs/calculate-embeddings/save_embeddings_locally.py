c3c118b723760d133b638aff99b6822159f40c76
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-05-12 17:25:06 +0200

    Fix function calling syntax

--- jobs/calculate-embeddings/save_embeddings_locally.py
+++ jobs/calculate-embeddings/save_embeddings_locally.py
@@ -6,7 +6,7 @@ OUTPUT_EMBEDDINGS_PATH = 'corpus_embeddings.pth'
 OUTPUT_ANALYSIS_DF_PATH = 'analysis_df.csv'
 
 if __name__ == "__main__":
-    analysis_df, corpus_embeddings = fetch_docs_and_embed
+    analysis_df, corpus_embeddings = fetch_docs_and_embed()
 
     torch.save(corpus_embeddings, OUTPUT_EMBEDDINGS_PATH)
     logging.info(f"Embeddings saved to '{OUTPUT_EMBEDDINGS_PATH}' with shape: {corpus_embeddings.shape}.")
